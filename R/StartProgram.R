54ac284e403e10ae959f41f2aed49b26df95fa69
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-08-26 13:35:36 +1200

    Pass min.val, max.val arguments to function

--- R/StartProgram.R
+++ R/StartProgram.R
@@ -8,7 +8,7 @@
 #' @examples
 StartProgram <- function(S, min.val, max.val){
   Fmax <- ifelse(F>0,1,0)
-  A <- simulated_annealing2(Fmax,niter = 450,0.009)[[1]]
+  A <- simulated_annealing2(Fmax, niter = 450,0.009, min.val, max.val)[[1]]
   E <- Fac_F_Final(A)[[1]]
   return(E)
 }
