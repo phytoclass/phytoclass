47ded422fdf08d2d664548273ee35b7c743556f2
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-08-29 11:29:38 +1200

    Insert global variable as function argument

--- R/Fac_F.R
+++ R/Fac_F.R
@@ -7,15 +7,16 @@
 #'
 #' @examples
 #'
-Fac_F <- function(Fn, S){
-  b <- crossprod(t(Weight_error(Fn)),t(Weight_error(S)))
-  C_new2 <-t(RcppML::nnls(crossprod(t(Weight_error(Fn))),b,cd_maxit = 100000,cd_tol =1e-6 ))
+Fac_F <- function(Fn, S, cm){
+  b <- crossprod(t(Weight_error(Fn, cm)),t(Weight_error(S, cm)))
+  C_new2 <-t(RcppML::nnls(crossprod(t(Weight_error(Fn, cm))), b, 
+                          cd_maxit = 100000,cd_tol =1e-6 ))
   C_new2 <- as.matrix(C_new2)
   Cn.s2 <- rowSums(C_new2)
   Cn2 <- C_new2/Cn.s2 #Row sums to one
   Cn2 <- as.matrix(Cn2)
   colnames(Cn2) <- rownames(F)
-  error <- norm(Weight_error(S) - (C_new2%*%Weight_error(Fn)), 'F')
+  error <- norm(Weight_error(S, cm) - (C_new2%*%Weight_error(Fn, )), 'F')
   return(list(Fn, error, Cn2))
 }
 
