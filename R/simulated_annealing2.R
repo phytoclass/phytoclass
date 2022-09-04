7d17b109496de28283defa483400b8d7a1576dec
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-09-05 11:54:22 +1200

    Include min.val max.val arguments

--- R/simulated_annealing2.R
+++ R/simulated_annealing2.R
@@ -28,13 +28,13 @@ simulated_annealing2 <- function(Fi, niter, step, S, min.val, max.val, cm, place
     Temp <- (1 - step)^(k) ### Set temp to decline with each iteration
     #consider random neighbour
     chlv <- Wrangling(s_c, min.val, max.val)[[4]]
-    new_neighbour <- Random_neighbour2(s_c, Temp, chlv, s_c, place)
+    new_neighbour <- Random_neighbour2(s_c, Temp, chlv, s_c, place, min.val, max.val)
     
     # considers 50 random neighbours and picks the one with the lowest error
     D <- list()
     for (i in 1:50){
       chlv <- Wrangling(s_c, min.val, max.val)[[4]]
-      D[[length(D)+1]] <- Random_neighbour2(s_c, Temp, chlv, s_c, place)
+      D[[length(D)+1]] <- Random_neighbour2(s_c, Temp, chlv, s_c, place, min.val, max.val)
     }
     Dn <- list()
     for (i in D){
@@ -67,7 +67,7 @@ simulated_annealing2 <- function(Fi, niter, step, S, min.val, max.val, cm, place
       N <- place[d]
       for (i in 1:50){
         chlv <- Wrangling(s_n, min.val, max.val)[[4]]
-        D[[length(D)+1]] <- Random_neighbour(s_n, Temp, chlv, s_n, N, place)
+        D[[length(D)+1]] <- Random_neighbour(s_n, Temp, chlv, s_n, N, place, min.val, max.val)
       }
       Dn <- list()
       for (i in D){
