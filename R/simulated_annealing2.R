a8e3ab201b01d22da3b0500baa734684f1b26c60
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-10-05 14:53:55 +1300

    Add in arguments

--- R/simulated_annealing2.R
+++ R/simulated_annealing2.R
@@ -48,10 +48,10 @@ simulated_annealing2 <- function(Fi, niter, step, S, min.val, max.val, cm, place
     # Applies the steepst descent algorithm to the new neighbour
     
     if (Temp > .2) {
-      new_neighbour <- SAALS(new_neighbour[[1]])
+      new_neighbour <- SAALS(new_neighbour[[1]], place, S, cm)
       
     }
-    else{new_neighbour <- SAALS2(new_neighbour[[1]])}
+    else{new_neighbour <- SAALS2(new_neighbour[[1]], place, S, cm)}
     
     
     minF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[1]] # mins and maxs
