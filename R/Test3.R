97f241307c11b686019c4e87602be6da31987205
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-10-05 14:18:48 +1300

    Include global variables in function calls

--- R/Test3.R
+++ R/Test3.R
@@ -1,11 +1,16 @@
 #' conduit between minimise_elements function and Fac_F_R
 #'
+#' @param F   
+#' @param place 
+#' @param S  
+#' @param cm
+#'
 #' @return
 #' @export
 #'
 #' @examples
-Test3 <- function(F, place){
-  F.old <- Fac_F(F)
+Test3 <- function(F, place, S, cm){
+  F.old <- Fac_F(F, S, cm)
   F.news <- Fac_F_RR3(F.old, place)
   F.new <- F.news[[1]]
   n <- F.news[[2]]
