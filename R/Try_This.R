d71115ed6b00bbc871a9193316a4435dd8830ecc
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-10-05 15:04:14 +1300

    Give explicit argument calls

--- R/Try_This.R
+++ R/Try_This.R
@@ -15,7 +15,7 @@ Try_This <- function(F, place, S, cm){
   F.new <- Fac_F(F, S, cm)
   F.initial <- F.new
   for (i in 1:5){ #should always be small. It would be nice to allow the 
-    F.new <- Minimise_elements(F.initial[[1]], place)
+    F.new <- Minimise_elements(F.initial[[1]], place, S, cm)
     loop = loop +1
     # print(loop)
     loop_2 <- 1
@@ -23,9 +23,9 @@ Try_This <- function(F, place, S, cm){
     while (F.new[[2]] > F.initial[[2]]) {
       loop_2 = loop_2+1
       #print(loop_2)
-      F.new <- Minimise_elements(F.initial[[1]])
-      if (loop_2 > 5){F.new <- Minimise_elements1(F.initial[[1]])} # If it doesn't work the first time, it randomises at a lower rate
-      if (loop_2 > 10){F.new <- Minimise_elements2(F.initial[[1]])} # and again, 
+      F.new <- Minimise_elements(F.initial[[1]], place, S, cm)
+      if (loop_2 > 5){F.new <-  Minimise_elements1(F.initial[[1]], place)} # If it doesn't work the first time, it randomises at a lower rate
+      if (loop_2 > 10){F.new <- Minimise_elements2(F.initial[[1]], place)} # and again, 
       
       #     if (loop_2 > 20){F.new <- Minimise_elements2(F.new[[1]])}
       if (loop_2 > 100){break} # it will continue for 100 itertions, and then stop
