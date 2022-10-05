a485b96b83de40a2680679a0a729bb8090d38379
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-10-06 12:19:15 +1300

    Include all arguments

--- R/Test2.R
+++ R/Test2.R
@@ -1,13 +1,17 @@
 #' conduit between minimise_elements function and Fac_F_R
+#' @param F   
+#' @param place
+#' @param S
+#' @param cm
 #'
 #' @return
 #' @export
 #'
 #' @examples
-Test2 <- function(F, place){
+Test2 <- function(F, place, S, cm){
   F.locs <- vector()
-  F.old <- Fac_F(F)
-  F.news <- Fac_F_RR2(F.old, place)
+  F.old <- Fac_F(F, S, cm)
+  F.news <- Fac_F_RR2(F.old, vary = place, place, S, cm)
   F.new <- F.news[[1]]
   n <- F.news[[2]]
   res <- list(F.new, n, F.old)
