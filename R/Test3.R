6aea098324294f933432066f92f91fc1d5f9d4c7
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-10-05 15:12:17 +1300

    Include S and cm arguments

--- R/Test3.R
+++ R/Test3.R
@@ -11,7 +11,7 @@
 #' @examples
 Test3 <- function(F, place, S, cm){
   F.old <- Fac_F(F, S, cm)
-  F.news <- Fac_F_RR3(F.old, place)
+  F.news <- Fac_F_RR3(F.old, place, S, cm)
   F.new <- F.news[[1]]
   n <- F.news[[2]]
   res <- list(F.new, n, F.old)
