14dfefd02db92ad6d34109556b68516953e24a04
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-10-06 11:57:08 +1300

    Include all arguments for function calls

--- R/Try_This2.R
+++ R/Try_This2.R
@@ -1,7 +1,8 @@
 #' sets how many iterations the algorithm should continue for x4. 
 #' This is the 'steepest descent' algorithm
 #' 
-#' @param F   
+#' @param F 
+#' @param place  
 #' @param S   
 #' @param cm     
 #'
@@ -9,12 +10,12 @@
 #' @export
 #'
 #' @examples
-Try_This2 <- function(F, S, cm){ 
+Try_This2 <- function(F, place, S, cm){ 
   loop <- 1
   F.new <- Fac_F(F, S, cm)
   F.initial <- F.new
   for (i in 1:2){ #should always be small. It would be nice to allow the 
-    F.new <- Minimise_elements(F.initial[[1]])
+    F.new <- Minimise_elements(F.initial[[1]], place, S, cm)
     loop = loop +1
     # print(loop)
     loop_2 <- 1
@@ -22,9 +23,9 @@ Try_This2 <- function(F, S, cm){
     while (F.new[[2]] > F.initial[[2]]) {
       loop_2 = loop_2+1
       #print(loop_2)
-      F.new <- Minimise_elements(F.initial[[1]])
-      if (loop_2 > 5){F.new <- Minimise_elements1(F.initial[[1]])} # If it doesn't work the first time, it randomises at a lower rate
-      if (loop_2 > 10){F.new <- Minimise_elements2(F.initial[[1]])} # and again, 
+      F.new <- Minimise_elements(F.initial[[1]], place, S, cm)
+      if (loop_2 > 5){F.new <- Minimise_elements1(F.initial[[1]], place)} # If it doesn't work the first time, it randomises at a lower rate
+      if (loop_2 > 10){F.new <- Minimise_elements2(F.initial[[1]], place)} # and again, 
       
       #     if (loop_2 > 20){F.new <- Minimise_elements2(F.new[[1]])}
       if (loop_2 > 100){break} # it will continue for 100 itertions, and then stop
