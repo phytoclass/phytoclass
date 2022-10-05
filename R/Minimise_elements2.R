b28251a31891cccca59ff46d49f2e29231bab5f7
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-10-06 12:36:54 +1300

    Include all argument calls

--- R/Minimise_elements2.R
+++ R/Minimise_elements2.R
@@ -8,14 +8,14 @@
 #'
 #' @examples
 Minimise_elements2 <- function(F, place, S, cm){   # A function that reduces every for every element that didn't reduce in index function
-  f <- Test2(F) # Calls index function
+  f <- Test2(F, place, S, cm) # Calls index function
   F.new <- f[[1]] # F matrix
   n <- f[[2]] #elements that reduce error
   if (is.null(n)){n <- place}
   F.old <- f[[3]] # old F matrix
   F.initial <- F.new # Fac_F new matrix
   # Fac_F new matrix
-  g <-Fac_F_RR2(F.new, place)
+  g <-Fac_F_RR2(F.new, vary = place, place, S, cm)
   if (g[[1]][[2]] < F.initial[[2]]){F.new <- g[[1]]}
   n <- g[[2]]
   res <- list(F.new,n)
