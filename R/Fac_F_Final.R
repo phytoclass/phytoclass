da5f397441bd265f4d58f3ccc60e34a72851a2f6
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-12-15 12:39:38 +1300

    Lots of changes: (i) calculation of "b" (ii) error calculation, (iii) introduction of plot into output (iv)

--- R/Fac_F_Final.R
+++ R/Fac_F_Final.R
@@ -13,14 +13,33 @@ Fac_F_Final <- function(Fn, S, S_Chl){
   F.sum <- Wrangle_F(Fn)[[2]]
   Fn <- Wrangle_F(Fn)[[1]]
   Fn <- Fn * F.sum
-  b <- crossprod(t(Fn),t(S))
-  C_new2 <-t(RcppML::nnls(crossprod(t(Fn)),b,cd_maxit = 1000000000,cd_tol =1e-18 ))
+  b <- crossprod(t(Weight_error(Fn, cm)),t(Weight_error(S, cm)))
+  C_new2 <-t(RcppML::nnls(crossprod(t(Weight_error(Fn, cm))), b,cd_maxit = 1000,cd_tol =1e-8 ))
   C_new2 <- as.matrix(C_new2)
   Cn.s2 <- rowSums(C_new2)
   Cn2 <- C_new2/Cn.s2
   Cn2 <- as.matrix(Cn2)
   Cn2 <- Cn2 * S_Chl
   colnames(Cn2) <- rownames(Fn)
-  error <- norm(S-(C_new2%*%Fn),'F')
-  return(list(Fn,error,Cn2))
+  error <- Metrics::rmse(S,(C_new2%*%Fn))
+  k <- Cn2
+  k <- as.data.frame(k)
+  k[,ncol(k)+1] <- 1:nrow(k)
+  cn <- colnames(k)
+  
+  cn <- cn[1:ncol(k)-1]
+  
+  PLE <- tidyr::pivot_longer(data = k,cols = cn,names_to = 'names',values_to = 'vals' )
+  colorBlindGrey8   <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
+                         "#F0E442", "#0072B2", "#D55E00", "#CC79A7","#009E73","#001E73","#013E73")
+  gr <- colnames(PLE)[[1]]
+  n <- ggplot(PLE, aes(x=UQ(as.name(gr)), y=vals, fill=names)) +
+    geom_area()+
+    scale_color_manual(values=colorBlindGrey8)+
+    scale_fill_manual(values=colorBlindGrey8)
+  G <- S - (C_new2%*%Fn)
+  gs <- colMeans(abs(G))/colSums(S)
+  
+  
+  return(list(Fn,error,Cn2,n,gs,G))
 }
