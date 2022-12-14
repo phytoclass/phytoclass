535d441955bdc59d24d9a9b1b985396d0b861c4b
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2022-12-15 12:34:06 +1300

    Changed: (i) arguments for nnls, (2) error calculation

--- R/Fac_F.R
+++ R/Fac_F.R
@@ -11,14 +11,15 @@
 #'
 Fac_F <- function(Fn, S, cm){
   b <- crossprod(t(Weight_error(Fn, cm)),t(Weight_error(S, cm)))
-  C_new2 <-t(RcppML::nnls(crossprod(t(Weight_error(Fn, cm))), b, 
-                          cd_maxit = 100000,cd_tol =1e-6 ))
+  C_new2 <-t(RcppML::nnls(crossprod(t(Weight_error(Fn, cm))),
+                          b, 
+                          cd_maxit = 1000,cd_tol =1e-8 ))
   C_new2 <- as.matrix(C_new2)
   Cn.s2 <- rowSums(C_new2)
   Cn2 <- C_new2/Cn.s2 #Row sums to one
   Cn2 <- as.matrix(Cn2)
   colnames(Cn2) <- rownames(F)
-  error <- norm(Weight_error(S, cm) - (C_new2%*%Weight_error(Fn, cm)), 'F')
+  error <- Metrics::rmse((S),C_new2%*%(Fn))
   return(list(Fn, error, Cn2))
 }
 
