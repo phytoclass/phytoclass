90255c71452389b44277cd9c6b3471536eeefe4a
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-01-11 10:55:17 +1300

    Replace Try_This() and Try_This2() by a single function with an argument for the number of loops.

--- R/SAALS2.R
+++ R/SAALS2.R
@@ -12,7 +12,7 @@
 #'
 #' @examples
 SAALS2 <- function(Ft, min.value, max.value, place, S, cm){
-  g <- Try_This2(Ft, place, S, cm)
+  g <- Try_This(Ft, place, S, cm, num.loops = 2)
   err <- g[[2]]
   g <- g[[1]]
   gchl <- g[,ncol(g)]
