8f88e2459e623579a2232e8a2e39d68bccd55def
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-01-20 13:44:05 +1300

    Add and process raw data for clusters, ratios, and min/max values

---
+++ data-raw/DATASET.R
@@ -0,0 +1,13 @@
+## code to prepare `DATASET` dataset goes here
+
+S <-read.csv("data-raw/Clusters_213.csv",header = TRUE,row.names = "X")
+S <- S[,14:length(S)-1]
+S <- S[,-c(18,11,2,1)]
+usethis::use_data(S, overwrite = TRUE)
+
+F <-read.csv("data-raw/Ratios_5.csv",header = TRUE,row.names = 'X') # Read pig:chl inital 
+F <- F[,-1]
+usethis::use_data(S, overwrite = TRUE)
+
+min_max <-read.csv("data-raw/Circumpolar_minmax.csv", header = TRUE) # Read pig:chl inital 
+usethis::use_data(min_max , overwrite = TRUE)
