fac4757ade69f217b548f7978ab1a5a13263babd
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-01-20 15:34:43 +1300

    rename Try_This function to Steepest_Descent

--- R/SAALS.R
+++ R/SAALS.R
@@ -16,7 +16,7 @@
 
 
 SAALS <- function(Ft, min.value, max.value, place, S, cm, num.loops){
-  g <- Try_This(Ft, place, S, cm, num.loops)
+  g <- Steepest_Descent (Ft, place, S, cm, num.loops)
   err <- g[[2]]
   g <- g[[1]]
   gchl <- g[,ncol(g)]
