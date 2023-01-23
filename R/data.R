81e20169e639791adf7ecf424512cb315bc44fb2
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-01-24 12:55:08 +1300

    Add documentation for built-in data

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' F data
+#'
+#'
+#' @format ## `F`
+#' A data frame with 10 rows and 15 columns:
+#' \describe{
+#'   \item{chl.c1}{XX}
+#'   \item{Per}{XX}
+#'   \item{X19but}{XX}
+#'   ...
+#' }
+#' @source XX
+"F"
+
+
+#' S data
+#'
+#'
+#' @format ## `S`
+#' A data frame with 17 rows and 16 columns:
+#' \describe{
+#'   \item{chl.c1}{XX}
+#'   \item{Per}{XX}
+#'   \item{X19but}{XX}
+#'   ...
+#' }
+#' @source XX
+"S"
+
+
+
+#' min and max data
+#'
+#'
+#' @format ## `min_max`
+#' A data frame with 68 rows and 4 columns:
+#' \describe{
+#'   \item{Class}{XX}
+#'   \item{Pig_Abbrev}{XX}
+#'   \item{min}{XX}
+#'   \item{max}{XX}#'   
+#'   ...
+#' }
+#' @source XX
+"min_max"
+
+
+
+
