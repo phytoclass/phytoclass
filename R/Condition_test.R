51777085f73578388c383d94717ac6b2b2dbe379
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-02-09 10:44:50 +1300

    New function

---
+++ R/Condition_test.R
@@ -0,0 +1,33 @@
+#' Calculate the condition number ...
+#'
+#' @param Fn
+#' @param min.val
+#' @param max.val
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+
+Condition_test <- function(Fn, min.val, max.val){
+  
+  condition_number <- function(Fd, min.val, max.val){
+    Fz <- vectorise(Fd)
+    rand <- vector()
+    for (i in 1:length(Fz)){
+      rand[[length(rand)+1]] <- stats::runif(1, min=min.val[i], max=max.val[i])
+    }
+    Fn <- Fd
+    Fn[Fn >0] <- rand
+    return(kappa(Fn %*% t(S)))
+  }
+  
+  sn <- vector()
+  for (i in 1:10000){
+    sn[[length(sn) + 1]] <- condition_number(F, min.val, max.val)
+  }
+  
+  return(mean(sn))
+  
+}
