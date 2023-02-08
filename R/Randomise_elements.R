5a291c3eae9f7d6a0d885c0d50ca8d73ad75a63b
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-02-08 15:32:03 +1300

    Rename rand() to Randomise_elements()

--- R/Randomise_elements.R
+++ R/Randomise_elements.R
@@ -1,4 +1,4 @@
-#' Randomisation function
+#' Randomise individual elements in the F matrix.
 #'
 #' @param x 
 #' @param na.rm 
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples
-rand <- function(x, min.scaler, max.scaler){
+Randomise_elements <- function(x, min.scaler, max.scaler){
   x <- ifelse(x<0.001, 0.001, x)
   round(runif(n=1, min = x*min.scaler, max = x*max.scaler), digits = 4)
 }
--- R/Replace_Rand.R
+++ R/Replace_Rand.R
@@ -13,7 +13,7 @@
 #' @examples
 Replace_Rand <- function(F, i, S, cm, min.scaler, max.scaler){
   # randomise first element of matrix  
-  F.new <- as.matrix(replace(F[[1]], i, rand(F[[1]][i], min.scaler, max.scaler))) 
+  F.new <- as.matrix(replace(F[[1]], i, Randomise_elements(F[[1]][i], min.scaler, max.scaler))) 
   F.new <- NNLS_MF(F.new, S, cm)
   # Which elements decrease the error? Store the location of the elements that decrease it   
   v <- which(F.new[[2]] < F[[2]])
