b3079223c1c3ab3c3e9c3d83bf114b5b07bb5515
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-03-02 12:51:44 +1300

    Change name for built-in: F -> Fm, s -> Sm

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,11 +3,11 @@
 # row names are in the first column
 S <- read.csv("data-raw/S_example.csv", header = TRUE, row.names = 1)
 # remove column at end containing "Shelf"
-S <- S[, -16]
-usethis::use_data(S, overwrite = TRUE)
+Sm <- S[, -16]
+usethis::use_data(Sm, overwrite = TRUE)
 
-F <- read.csv("data-raw/F_example.csv", header = TRUE, row.names = 1)
-usethis::use_data(F, overwrite = TRUE)
+Fm <- read.csv("data-raw/F_example.csv", header = TRUE, row.names = 1)
+usethis::use_data(Fm, overwrite = TRUE)
 
 min_max <-read.csv("data-raw/Circumpolar_minmax.csv", header = TRUE) # Read pig:chl inital 
 usethis::use_data(min_max , overwrite = TRUE)
