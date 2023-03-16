95e3b8b0f60f36c1bf63a774e5253e3b3472273a
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-03-16 13:29:38 +1300

    First commit of Steepest_Desc() function

---
+++ R/Steepest_Desc.R
@@ -0,0 +1,49 @@
+#' Stand-alone version of steepest descent algorithm
+#'
+#' @param F
+#' @param S 
+#' @param num.loops 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+Steepest_Desc <-  function (F, S, num.loops) 
+{
+  L <- Matrix_checks(S, F)
+  S <- as.matrix(L[[1]])
+  S_Chl <- S[, ncol(S)]
+  cm <- Bounded_weights(S)
+  S <- Normalise_S(S)
+  F <- as.matrix(L[[2]])
+  place <- which(F > 0)
+  
+  loop <- 1
+  F.new <- NNLS_MF(F, S, cm)
+  F.initial <- F.new
+  for (i in 1:num.loops) {
+    F.new <- Minimise_elements(F.initial[[1]], place, S, 
+                               cm)
+    loop = loop + 1
+    loop_2 <- 1
+    while (F.new[[2]] > F.initial[[2]]) {
+      loop_2 = loop_2 + 1
+      F.new <- Minimise_elements(F.initial[[1]], place, 
+                                 S, cm)
+      if (loop_2 > 5) {
+        F.new <- Minimise_elements1(F.initial[[1]], place, 
+                                    S, cm)
+      }
+      if (loop_2 > 10) {
+        F.new <- Minimise_elements2(F.initial[[1]], place, 
+                                    S, cm)
+      }
+      if (loop_2 > 100) {
+        break
+      }
+    }
+    F.initial <- F.new
+  }
+  F.new <- NNLS_MF_Final(F.new, S, S_Chl, cm)
+  return(F.new)
+}
