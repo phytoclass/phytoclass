fa568cc4ec1a67d23e7842e7c5abb948ccf78c46
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-04-26 16:27:07 +1200

    Bring back previous default min_max

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -9,5 +9,6 @@ usethis::use_data(Sm, overwrite = TRUE)
 Fm <- read.csv("data-raw/F_example.csv", header = TRUE, row.names = 1)
 usethis::use_data(Fm, overwrite = TRUE)
 
-min_max <-read.csv("data-raw/Circumpolar_minmax.csv", header = TRUE) # Read pig:chl inital 
+min_max <-read.csv("data-raw/Circumpolar_minmax.csv", header = TRUE) # Read pig:chl inital
+min_max <- min_max[, c("Class", "Pig_Abbrev", "min", "max")]
 usethis::use_data(min_max , overwrite = TRUE)
