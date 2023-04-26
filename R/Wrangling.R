c66fecca06949d7917d5a6571e2124aa741a0722
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-04-26 16:00:37 +1200

    Assign chlv result in a new way

--- R/Wrangling.R
+++ R/Wrangling.R
@@ -12,14 +12,16 @@
 Wrangling <- function(Fl, min.val, max.val){
   Fd <- Fl
   Fmin <- as.matrix(Fd)   #### Set up Fmin matrix
+  Fmin <- Fmin[,1:ncol(Fmin)-1]
   Fmin[Fmin>0] <- min.val  # set all non-zero elements to the minimum values (imported from csv )
   Fmax <- as.matrix(Fd)
+  Fmax <- Fmax[,1:ncol(Fmax)-1]
   Fmax[Fmax>0] <- max.val
   chlv <- Fd[,ncol(Fd)]  ##### The chlorophyll values once weighted to rowsums for initial F matrix
   Fmin <- Fmin * chlv #### multiply the minimum value by weighted chlorophyll to updated ratios
-  Fmin[,ncol(Fmin)] <- chlv  #### Reassign correct initial chl values
+  Fmin <- cbind(Fmin,chlv)  #### Reassign correct initial chl values
   Fmax <- Fmax * chlv  #### same for max values
-  Fmax[,ncol(Fmax)] <- chlv
+  Fmax <- cbind(Fmax,chlv)
   
   Fmin <- vectorise(Fmin[,1:ncol(Fmin)-1]) #### vectorise function outputs all non-zero elements as a vector (excluding chl column)
   Fmax <- vectorise(Fmax[,1:ncol(Fmax)-1]) #### vectorise function outputs all non-zero elements as a vector (excluding chl column)
@@ -28,5 +30,4 @@ Wrangling <- function(Fl, min.val, max.val){
   res <- list(Fmin, Fmax, SE,chlv)
   return(res)
 }
-
 
