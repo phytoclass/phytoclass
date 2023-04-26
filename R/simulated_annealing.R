e4c857239cc066c48302c2c643080b04068717a7
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-04-26 16:11:56 +1200

    Bunch of changes, with lots of formatting changes

--- R/simulated_annealing.R
+++ R/simulated_annealing.R
@@ -32,145 +32,159 @@ simulated_annealing <- function(S,
                                 niter = 500,
                                 step = 0.009,
                                 weight.upper.bound = 30){
-  
-  # Use default F value
-  if(is.null(F)) {
+  if (is.null(F)) {
     F <- phytoclass::Fm
   }
-
-  if(do_matrix_checks) {
+  
+  if (do_matrix_checks) {
     L <- Matrix_checks(S, F)
-    S <- as.matrix(L[[1]])  
-    F <- as.matrix(L[[2]])    
+    S <- as.matrix(L[[1]])
+    F <- as.matrix(L[[2]])
   }
- 
-  S_Chl <- S[, ncol(S)]  # used at end of function
-  S <- Normalise_S(S)  
+  
+  S_Chl <- S[, ncol(S)]
+  S <- Normalise_S(S)
   cm <- Bounded_weights(S, weight.upper.bound)
- 
-  place <- which(F > 0)
+  place <- which(F[,1:ncol(F)-1] > 0)
   
   if (is.null(user_defined_min_max)) {
-    K <- Default_min_max(phytoclass::min_max, F, place)
+    K <- Default_min_max(phytoclass::min_max, F[,1:ncol(F)-1], place)
     min.val <- K[[1]]
     max.val <- K[[2]]
-  } else {
+  }
+  else {
     min.val <- user_defined_min_max$min
     max.val <- user_defined_min_max$max
-    if (length(min.val) != length(place)) {
-      message(paste0("\nNumber of rows for user_defined_min_max = ", length(min.val)))
-      message(paste0("Length of place = ", length(place)))
-      stop("\nThese do not match.")
-    }
+    # if (length(min.val) != length(place)) {
+    #   message(paste0("\nNumber of rows for user_defined_min_max = ", 
+    #                  length(min.val)))
+    #   message(paste0("Length of place = ", length(place)))
+    #   stop("\nThese do not match.")
+    # }
   }
   
   condition.test <- Condition_test(S, F, min.val, max.val)
-  cat(paste0("\nCondition number = ", round(condition.test), "\n\n"))
-  if(condition.test > 10^5) {
+  cat(paste0("\nCondition number = ", round(condition.test), 
+             "\n\n"))
+  
+  if (condition.test > 10^5) {
     stop("Condition number of S matrix greater than 100 000\n")
   }
- 
-  Fi <- ifelse(F > 0, 1, 0)
   
+  Fi <- ifelse(F > 0, 1, 0)
   SE <- vectorise(Fi)
   nc <- NNLS_MF(Fi, S, cm)
-  
-  s_b <- s_c <- s_n <- nc[[1]]  # sets initial values 
+  s_b <- s_c <- s_n <- nc[[1]]
   f_b <- f_c <- f_n <- nc[[2]]
-
-  for (k in 1:niter) #Set up loop (niter = number of iterations
-  {   
-    Temp <- (1 - step)^(k) ### Set temp to decline with each iteration
-    #consider random neighbour
+  
+  for (k in 1:niter) {
+    Temp <- (1 - step)^(k)
     chlv <- Wrangling(s_c, min.val, max.val)[[4]]
-    new_neighbour <- Random_neighbour2(s_c, Temp, chlv, s_c, place, S, cm, min.val, max.val)
-    
-    # considers 50 random neighbours and picks the one with the lowest error
-    D <- list()
-    for (i in 1:70){
-      chlv <- Wrangling(s_c, min.val, max.val)[[4]]
-      D[[length(D)+1]] <- Random_neighbour2(s_c, Temp, chlv, s_c, place, S, cm, min.val, max.val)
+    new_neighbour <- Random_neighbour2(s_c, Temp, chlv, s_c, 
+                                       place, S, cm, min.val, max.val)
+    if (k > niter-20){
+      for (i in 1:300){
+        chlv <- Wrangling(s_c, min.val, max.val)[[4]]
+        D[[length(D)+1]] <- Random_neighbour2(s_c, Temp,
+                                              chlv, s_c, place, S, cm, min.val, max.val)
+      }
+    }
+    else{
+      for (i in 1:120){
+        chlv <- Wrangling(s_c, min.val, max.val)[[4]]
+        D[[length(D)+1]] <- Random_neighbour2(s_c, Temp,
+                                              chlv, s_c, place, S, cm, min.val, max.val)
+      }
     }
     Dn <- list()
-    for (i in D){
-      Dn[[length(Dn)+1]] <-  i[[2]]
+    for (i in D) {
+      Dn[[length(Dn) + 1]] <- i[[2]]
     }
-    
     nk <- which.min(Dn)
-    
     new_neighbour <- D[[nk]]
-    
-    # Applies the steepst descent algorithm to the new neighbour
-    
-    if (Temp > .3) {
-      new_neighbour <- SAALS(new_neighbour[[1]], min.val, max.val,
-                             place, S, cm, num.loops = 10)
+    if (Temp > 0.3) {
+      new_neighbour <- SAALS(new_neighbour[[1]], min.val, 
+                             max.val, place, S, cm, num.loops = 10)
     }
     else {
-      new_neighbour <- SAALS(new_neighbour[[1]], min.val, max.val, 
-                              place, S, cm, num.loops = 2)
+      new_neighbour <- SAALS(new_neighbour[[1]], min.val, 
+                             max.val, place, S, cm, num.loops = 2)
     }
-    
-    
-    minF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[1]] # mins and maxs
+    minF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[1]]
     maxF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[2]]
-    
     s_n <- new_neighbour[[1]]
     f_n <- new_neighbour[[2]]
-    
-    #Shows which are below / above the min and max values
     loop <- 1
-    d <- which(vectorise(s_n[,1:(ncol(s_n)-1)])<minF | vectorise(s_n[,1:(ncol(s_n)-1)]) > maxF) 
-    while( length(d) > 0){
-      N <- place[d]
-      for (i in 1:70){
-        chlv <- Wrangling(s_n, min.val, max.val)[[4]]
-        D[[length(D)+1]] <- Random_neighbour(s_n, Temp, chlv, s_n, N, place, S, cm, min.val, max.val)
+    d <- which(vectorise(s_n[, 1:(ncol(s_n) - 1)]) < minF | 
+                 vectorise(s_n[, 1:(ncol(s_n) - 1)]) > maxF)
+    while (length(d) > 0) {
+        if (k > niter-20){
+          N <- place[d]
+          for (i in 1:300){
+            chlv <- Wrangling(s_n, min.val, max.val)[[4]]
+            D[[length(D)+1]] <- Random_neighbour(s_n, Temp, chlv, s_n, N, place, S, cm, min.val, max.val)
+          }
+          Dn <- list()
+          for (i in D){
+            Dn[[length(Dn)+1]] <-  i[[2]]
+          }
+          
+          nk <- which.min(Dn)
+          
+          new_neighbour <- D[[nk]]
+          s_n <- new_neighbour[[1]]
+          f_n <- new_neighbour[[2]]
+          
+          minF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[1]]
+          maxF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[2]]
+          
+          d <- which(vectorise(s_n[,1:(ncol(s_n)-1)])<minF | vectorise(s_n[,1:(ncol(s_n)-1)]) > maxF) 
+          
+        }
+      else{
+        N <- place[d]
+        for (i in 1:120){
+          chlv <- Wrangling(s_n, min.val, max.val)[[4]]
+          D[[length(D)+1]] <- Random_neighbour(s_n, Temp, chlv, s_n, N, place, S, cm, min.val, max.val)
+        }
+        Dn <- list()
+        for (i in D){
+          Dn[[length(Dn)+1]] <-  i[[2]]
+        }
+        
+        nk <- which.min(Dn)
+        
+        new_neighbour <- D[[nk]]
+        s_n <- new_neighbour[[1]]
+        f_n <- new_neighbour[[2]]
+        
+        minF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[1]]
+        maxF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[2]]
+        
+        d <- which(vectorise(s_n[,1:(ncol(s_n)-1)])<minF | vectorise(s_n[,1:(ncol(s_n)-1)]) > maxF) 
+        
       }
-      Dn <- list()
-      for (i in D){
-        Dn[[length(Dn)+1]] <-  i[[2]]
-      }
-      
-      nk <- which.min(Dn)
-      
-      new_neighbour <- D[[nk]]
-      s_n <- new_neighbour[[1]]
-      f_n <- new_neighbour[[2]]
-      
-      minF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[1]]
-      maxF <- Wrangling(new_neighbour[[1]], min.val, max.val)[[2]]
-      
-      d <- which(vectorise(s_n[,1:(ncol(s_n)-1)])<minF | vectorise(s_n[,1:(ncol(s_n)-1)]) > maxF) 
-      
       
     }
     
-    # Difference in error
-    A = target(f_n)/target(f_c) 
+    A = target(f_n)/target(f_c)
     diff <- f_n - f_c
-    if (f_n < f_c || exp(-(f_n - f_c)) < stats::runif(1, 0, 1)) {   # metropolis criterion we can switch off and on.
+    if (f_n < f_c || exp(-(f_n - f_c)) < stats::runif(1, 
+                                                      0, 1)) {
       s_c <- s_n
       f_c <- f_n
     }
-
     print(paste("Current error: ", round(f_c, 4)))
     print(paste("Neighbour's error: ", round(f_n, 4)))
-    print(paste("Temperature (%): ", round(Temp*100, 2)))
-    print(" ")    
-    
-    # update best state
+    print(paste("Temperature (%): ", round(Temp * 100, 2)))
+    print(" ")
     if (f_n < f_b) {
       s_b <- s_n
-      f_b <- f_n       
+      f_b <- f_n
     }
-    
   }
+  
   res <- list(s_b, f_b)
   A <- res[[1]]
- 
   NNLS_MF_Final(A, S, S_Chl, cm)
-  
 }
-
-
