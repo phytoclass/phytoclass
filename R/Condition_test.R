83022f80986b5d069496730369e67e3e5c50ccc1
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-08-29 10:22:33 +1200

    Code for null values for min.val and max.val

--- R/Condition_test.R
+++ R/Condition_test.R
@@ -11,21 +11,25 @@
 #' @examples
 
 
-Condition_test <- function(S, Fn, min.val, max.val){
-  
-  condition_number <- function(S, Fd, min.val, max.val){
+Condition_test <- function(S, Fn, min.val=NULL, max.val=NULL){
+   if (is.null(min.val) & is.null(max.val)) {
+    place <- which(Fn > 0)
+    K <- Default_min_max(phytoclass::min_max, Fn, place)
+    min.val <- K[[1]]
+    max.val <- K[[2]]
+  }
+  condition_number <- function(S, Fd, min.val, max.val) {
     Fz <- vectorise(Fd)
     rand <- vector()
-    for (i in 1:length(Fz)){
-      rand[[length(rand)+1]] <- stats::runif(1, min=min.val[i], max=max.val[i])
+    for (i in 1:length(Fz)) {
+      rand[[length(rand) + 1]] <- stats::runif(1, min = min.val[i], 
+                                               max = max.val[i])
     }
     Fn <- Fd
-    Fn[Fn >0] <- rand
+    Fn[Fn > 0] <- rand
     return(kappa(Fn %*% t(S)))
   }
-  
-  sn <- replicate(n = 10000, condition_number(S, Fn, min.val, max.val))
-  
+  sn <- replicate(n = 10000, condition_number(S, Fn, min.val, 
+                                              max.val))
   return(mean(sn))
-  
 }
