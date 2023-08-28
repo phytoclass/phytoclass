a30b138e53f5d53f5230d78b1fcd74c6370c8904
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-08-29 11:24:44 +1200

    Add in min_cluster_size argument to function

--- R/Cluster.R
+++ R/Cluster.R
@@ -6,68 +6,74 @@
 #' @export
 #'
 #' @examples
-Cluster <- function(Data) {
-  standardise <- function(Data) {
+
+
+Cluster <- function(Data, min_cluster_size){
+  
+  standardise <- function(Data){
     b <- Data
-    b <- b[, 1:ncol(b) - 1]
-    Chl <- Data[, ncol(Data)]
-    b[b == 0] <- 1e-6
-    b <- b / Chl
-    v <- lapply(b, bestNormalize::boxcox)
+    b <- b[,1:ncol(b)-1]
+    Chl <- Data[,ncol(Data)]
+    b[b==0] <- 1e-6
+    b <- b/Chl
+    v <- lapply(b,bestNormalize::boxcox)
     return(v)
   }
-
+  
+  
   v <- standardise(Data)
-
+  
   L <- length(v)
-
-  ndf <- list()
-  for (i in 1:L) {
-    ndf[[length(ndf) + 1]] <- data.frame(v[[i]][1])
+  
+  ndf <- list() 
+  for (i in 1:L){
+    ndf[[length(ndf)+1]] <- data.frame(v[[i]][1])
   }
-
+  
   S <- Data
   ndf <- do.call("cbind", ndf)
-  colnames(ndf) <- colnames(S[, ncol(S) - 1])
-
-
-  mscluster <- stats::dist(ndf, method = "manhattan")
-  mv.hclust <- stats::hclust(mscluster, method = "ward.D2")
-
+  colnames(ndf) <- colnames(S[,ncol(S)-1])
+  
+  
+  mscluster <- dist(ndf, method = "manhattan")
+  mv.hclust <- hclust(mscluster, method = "ward.D2")
+  
   plot(mv.hclust)
-
+  
   ev.clust <- Data
+  #Change the minClusterSize argument below to change the number of clusters. 
+  #You might want to play around with it if you want ~ 6 clusters. 
+  #You could try setting it at 1/6th of the total sample number :)
   dynamicCut <- dynamicTreeCut::cutreeDynamic(mv.hclust,
     cutHeight = 70,
-    minClusterSize = 12,
+    minClusterSize = min_cluster_size,
     method = "hybrid",
-    distM = as.matrix(stats::dist(ndf, method = "manhattan")),
-    deepSplit = 4,
-    pamStage = TRUE,
-    pamRespectsDendro = TRUE,
-    useMedoids = FALSE,
-    maxDistToLabel = NULL,
+    distM = as.matrix(dist(ndf, method = "manhattan")), deepSplit = 4,
+    pamStage = TRUE, pamRespectsDendro = TRUE,
+    useMedoids = FALSE, maxDistToLabel = NULL,
     maxPamDist = 50,
-    respectSmallClusters = TRUE,
+    respectSmallClusters = TRUE
   )
-
+  
   # NULL assignment to stop NOTE during the package "Check"
   #  -  no visible binding for global variable
   Clust <- NULL
+  
   ev.clust$Clust <- dynamicCut
-
+  
   L2 <- length(unique(ev.clust$Clust))
   L <- list()
-  for (i in 1:L2) {
-    L[[length(L) + 1]] <- dplyr::filter(ev.clust, Clust == i)
+  for (i in 1:L2){
+    L[[length(L)+1]] <- dplyr::filter(ev.clust,Clust==i)
   }
-
+  
   L
-
+  
   e <- numeric()
-  for (i in 1:length(L)) {
-    e[[length(e) + 1]] <- length(L[[i]][[1]])
+  for (i in 1:length(L)){
+    e[[length(e)+1]] <- length(L[[i]][[1]])
   }
-
+  
+  
   return(list(L, plot(mv.hclust)))
 }
