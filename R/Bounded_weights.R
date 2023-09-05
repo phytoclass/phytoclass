6434fb7320dd8be7711b8ae5e5020ef34e5b5aaa
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-09-06 10:59:37 +1200

    Fill out function help

--- R/Bounded_weights.R
+++ R/Bounded_weights.R
@@ -1,13 +1,15 @@
 #' Add weights to the data, bound at a maximum.
 #' 
-#' @param S XX         
-#' @param weight.upper.bound  XX        
+#' @param S   Sample data matrix – a matrix of pigment samples
+#' @param weight.upper.bound  Upper bound for weights (default is 30)        
 #'
-#' @return A matrix
+#' @return A vector with upper bounds for weights
 #' @export
 #'
 #' @examples
-Bounded_weights <-function(S, weight.upper.bound){
+#' Bounded_weights(Sm, weight.upper.bound = 30)
+#' 
+Bounded_weights <-function(S, weight.upper.bound = 30){
   n <- colMeans(S)
   S <- n^-1
   S <- ifelse(S > weight.upper.bound, weight.upper.bound, S)
