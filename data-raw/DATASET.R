6dccdba6aa1817762bcf84f784ab4fe71286576d
Andy McKenzie <Andy.McKenzie@niwa.co.nz>
2023-09-06 09:48:36 +1200

    No history for revised built-in data objects: Sm, Rm, min_max

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,14 +1,9 @@
 ## code to prepare `DATASET` dataset goes here
 
 # row names are in the first column
-S <- read.csv("data-raw/S_example.csv", header = TRUE, row.names = 1)
+# S <- read.csv("data-raw/S_example.csv", header = TRUE, row.names = 1)
 # remove column at end containing "Shelf"
-Sm <- S[, -16]
-usethis::use_data(Sm, overwrite = TRUE)
+# Sm <- S[, -16]
+# usethis::use_data(Sm, overwrite = TRUE)
 
-Fm <- read.csv("data-raw/F_example.csv", header = TRUE, row.names = 1)
-usethis::use_data(Fm, overwrite = TRUE)
 
-min_max <-read.csv("data-raw/Circumpolar_minmax2.csv", header = TRUE) # Read pig:chl inital
-min_max <- min_max[, c("Class", "Pig_Abbrev", "min", "max")]
-usethis::use_data(min_max , overwrite = TRUE)
