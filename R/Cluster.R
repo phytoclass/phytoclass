7a2e7f8e3ccf849e559d426883f4fdda21624ad9
aasimkhan02 <aasimkroc@gmail.com>
2025-05-29 12:28:08 +0530

    Fix #24: rename clustering param minClusterSize -> minSamplesPerCluster

--- R/Cluster.R
+++ R/Cluster.R
@@ -1,7 +1,7 @@
 #' Cluster things
 #'
 #' @param Data S (sample) matrix
-#' @param min_cluster_size the minimum size required for a cluster
+#' @param minSamplesPerCluster the minimum number of samples required for a cluster
 #'
 #' @return A named list of length two. The first element "cluster.list"
 #'  is a list of clusters, and the second element "cluster.plot" the 
@@ -12,7 +12,7 @@
 #' Cluster.result <- Cluster(Sm, 14)
 #' Cluster.result$cluster.list
 #' plot(Cluster.result$cluster.plot)
-Cluster <- function(Data, min_cluster_size) {
+Cluster <- function(Data, minSamplesPerCluster) {
   
   standardise <- function(Data) {
     b <- Data
@@ -42,12 +42,12 @@ Cluster <- function(Data, min_cluster_size) {
   mv.hclust <- stats::hclust(mscluster, method = "ward.D2")
 
   ev.clust <- Data
-  # Change the minClusterSize argument below to change the number of clusters.
+  # Change the minSamplesPerCluster argument below to adjust how many samples 
   # You might want to play around with it if you want ~ 6 clusters.
   # You could try setting it at 1/6th of the total sample number :)
   dynamicCut <- dynamicTreeCut::cutreeDynamic(mv.hclust,
     cutHeight = 70,
-    minClusterSize = min_cluster_size,
+    minClusterSize = minSamplesPerCluster,
     method = "hybrid",
     distM = as.matrix(stats::dist(ndf, method = "euclidean")), 
     deepSplit = 4,
