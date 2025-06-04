cb5a88ff6a3bfa48311dab0b7c1967ff04523eb0
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-04 13:29:21 -0400

    add description info

--- R/Wrangling.R
+++ R/Wrangling.R
@@ -1,14 +1,20 @@
+#' Wrangle data to vectors
+#' 
 #' Converts data-types and selects data for randomisation in 
 #' the simulated annealing algorithm
 #' 
 #' @keywords internal
 #'
-#' @param Fl      The initial F matrix (i.e. pigment ratio matrix)
-#' @param min.val The minimum values for each pigment ratio 
-#' @param max.val The maximum values for each pigment ratio 
+#' @param Fl      A matrix of the initial F matrix (i.e. pigment ratio matrix)
+#' @param min.val A vector of the minimum values for each non-zero pigment ratios 
+#' @param max.val A vector of the maximum values for each non-zero pigment ratios
 #'
 #' @return
-#'
+#'     A list containing following components:
+#'     - A vector Fmin with the minimum pigment ratio values
+#'     - A vector Fmax with the maximum pigment ratio values
+#'     - A vector SE with the current pigment ratio values
+#'     - A vector chlv with the pigment ratio values for the last column in Fl
 #' @examples
 Wrangling <- function(Fl, min.val, max.val) {
   
