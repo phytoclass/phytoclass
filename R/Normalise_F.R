aaccfe25b337309d187d12f165fe2f2f3edffff0
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-04 14:26:10 -0400

    rm old code

--- R/Normalise_F.R
+++ R/Normalise_F.R
@@ -16,15 +16,3 @@ Normalise_F <- function(Fmat) {
   F_1   <- F_1 / F.sum               # divide by sum
   return(list(F_1, F.sum))
 }
-
-
-# Normalise_F2 <- function(Fmat){
-#   F_1<- Fmat
-#   Fchl <- F_1[,ncol(F_1)]
-#   F_1 <- F_1 / Fchl
-#   F.sum <- rowSums(F_1)
-#   #F_1 <- F/norm(F,'F')
-#   F_1 <- F_1 / F.sum
-#   F_1m <- as.matrix(F_1)
-#   return(list(F_1m,F.sum))
-# }
