4cac824724fae608388c173e1a1cead06ca1da5a
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-05 13:07:22 -0400

    update function for better error message

--- R/Default_min_max.R
+++ R/Default_min_max.R
@@ -11,8 +11,7 @@
 #' @return
 #'
 #' @examples
-
-
+#' 
 Default_min_max <- function(min_max, Fmat, place){
   k <- list()
   for (i in place){
@@ -44,4 +43,44 @@ Default_min_max <- function(min_max, Fmat, place){
   max <- min_max[vecs,4]
   
   return(list(min,max))
+}
+
+Default_min_max <- function(min_max, Fmat) {
+  
+  # selects all non-zero pigment ratios and indexes the phyto names and pigments
+  k <- which(Fmat > 0, TRUE)
+  RName <- rownames(Fmat)[k[, 1]]
+  CName <- colnames(Fmat)[k[, 2]]
+
+  # Find indices in min_max matching each taxa-pigment pair;
+  # throw error if any pair is missing in min_max
+  vecs    <- vector(length = nrow(k))
+
+  for (i in seq_len(nrow(k))) {
+    idx <- which(RName[i] == min_max[, 1] & CName[i] == min_max[, 2])
+    if (length(idx) == 0) {
+      idx <- -1
+    }
+    vecs[i] <- idx
+  }
+
+  if (any(vecs == -1)) {
+    mis_idx <- which(vecs == -1)
+    stop(
+      call. = FALSE,
+      c(
+        "Your F matrix includes a missing taxa-pigment pair in the `min_max` matrix.\n",
+        sprintf(
+          "%-20.20s : %7.7s\n",
+          RName[mis_idx],
+          CName[mis_idx]
+        )
+      )
+    )
+  }
+
+  min <- min_max[vecs, 3]
+  max <- min_max[vecs, 4]
+
+  return(list(min, max))
 }
