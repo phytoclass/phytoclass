8394cbb3c090ca8e1304140991d5031213e0bdb6
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-05 13:09:32 -0400

    rm old code

--- R/Default_min_max.R
+++ R/Default_min_max.R
@@ -12,43 +12,10 @@
 #'
 #' @examples
 #' 
-Default_min_max <- function(min_max, Fmat, place){
-  k <- list()
-  for (i in place){
-    k[[length(k)+1]] <- arrayInd(i, dim(Fmat))
-  }
-  
-  RName <- vector()
-  CName <- vector()
-  for (i in 1:length(k)){
-    RName[[length(RName)+1]] <- rownames(Fmat)[k[[i]][,1]]
-    CName[[length(CName)+1]] <- colnames(Fmat)[k[[i]][,2]]
-  }
-  
-  
-  # Find indices in min_max matching each taxa-pigment pair;
-  # throw error if any pair is missing in min_max
-  vecs <- vector()
-  for (i in 1:length(k)){
-    idx <- which(RName[[i]] == min_max[,1] & CName[[i]] == min_max[,2])
-    if (length(idx) == 0) {
-      stop(paste0("Your F matrix includes an unexpected taxa-pigment pair for ", 
-                  RName[[i]], " - ", CName[[i]], 
-                  ". This pair is not in the min_max matrix."))
-    }
-    vecs[[length(vecs)+1]] <- idx
-  }
-  
-  min <- min_max[vecs,3]
-  max <- min_max[vecs,4]
-  
-  return(list(min,max))
-}
-
 Default_min_max <- function(min_max, Fmat) {
   
   # selects all non-zero pigment ratios and indexes the phyto names and pigments
-  k <- which(Fmat > 0, TRUE)
+  k     <- which(Fmat > 0, TRUE)
   RName <- rownames(Fmat)[k[, 1]]
   CName <- colnames(Fmat)[k[, 2]]
 
@@ -58,12 +25,15 @@ Default_min_max <- function(min_max, Fmat) {
 
   for (i in seq_len(nrow(k))) {
     idx <- which(RName[i] == min_max[, 1] & CName[i] == min_max[, 2])
+    
+    # add -1 if missing phyto - pigment pair
     if (length(idx) == 0) {
       idx <- -1
     }
     vecs[i] <- idx
   }
 
+  # collect all missing pairs and error
   if (any(vecs == -1)) {
     mis_idx <- which(vecs == -1)
     stop(
