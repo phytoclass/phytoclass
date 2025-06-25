b864dea456cfb569e47435950c7cd07bc9acfd28
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-06-25 14:43:21 -0400

    edit maybe need to redue

--- R/Random_neighbour.R
+++ R/Random_neighbour.R
@@ -18,46 +18,48 @@
 #'
 #' @examples
 Random_neighbour <- function(Fn, Temp, chlv, s_c, N, place, S, cm, min.val, max.val){
-  k <- match(N,place)
+  
+  k   <- match(N, place)
   s_c <- s_c[N]
-  SE <- Wrangling(Fn, min.val, max.val)[[3]] #### vectorise function outputs all non-zero elements as a vector (excluding chl column)
-  SE <- SE[k]
-  minF <- Wrangling(Fn, min.val, max.val)[[1]]
-  minF <- minF[k]
-  maxF <- Wrangling(Fn, min.val, max.val)[[2]]
-  maxF <- maxF[k]
-  ki <- maxF-minF
-  rand <- round(runif(n = length(s_c), -1, 1),4)
-  SA <- (SE + (Temp) *ki*rand)
-  SA <- as.vector(unlist(SA))
-  d <- which(SA < minF | SA > maxF)
-  length(d)
+  
+  # vectorise function outputs all non-zero elements as a vector
+  # (excludes Tchla column)
+  wrangled <- Wrangling(Fn, min.val, max.val)
+  minF     <- wrangled[[1]][k]
+  maxF     <- wrangled[[2]][k]
+  SE       <- wrangled[[3]][k]
+  
+  rand <- round(runif(n = length(s_c), -1, 1), 4)
+  SA   <- (SE + Temp * (maxF - minF) * rand)
+  
+  # F matrix indexes that are outside the range of min and max
+  d    <- which(SA < minF | SA > maxF)
+  maxd <- maxF
+  mind <- minF
+  
   loop <- 1
-  while (length(d) >0){
-    loop = loop +1
-    nr <- round(runif(length(d),-1,1),4)
-    minr <- as.vector(unlist(minF))
-    maxr <- as.vector(unlist(maxF))
-    mind <- minr[d]
-    maxd <- maxr[d]
-    kir <- maxd-mind
-    SA2 <- (SE[d] +(Temp)*kir*nr)
-    SA[d] <- SA2 
-    d <- which(SA < minF | SA > maxF)
-    #print(loop) # if loop is > 3000, just select it from a uniform distribution between the max and min
-    if (loop > 50){
-      nn <- (minF[d]+maxF[d])/2
-      f <- round(runif(n=length(d),(minF[d]*1.2),(maxF[d]*0.80)),4)
+  while (length(d) > 0) {
+    loop  <- loop + 1
+    nr    <- round(runif(length(d), -1, 1), 4)
+    SA2   <- (SE[d] + Temp * maxd[d] - mind[d] * nr)
+    SA[d] <- SA2
+    d     <- which(SA < minF | SA > maxF)
+    
+    if (loop > 50) {
+      f     <- round(runif(n = length(d), (minF[d] * 1.2), (maxF[d] * 0.80)), 4)
       SA[d] <- f
-      d <- which(SA < minF | SA > maxF)
+      d     <- which(SA < minF | SA > maxF)
     }
+    
   }
-  Fn <- Fn[,1:ncol(Fn)-1] #### If error is lower, reassign the values
-  Fn[N] <- SA
-  Fn <- cbind(Fn,chlv)
-  colnames(Fn) <- colnames(F)
-  F.n <- NNLS_MF(Fn, S, cm)
-  return(F.n)
+  
+  
+  Fn           <- Fn[, -ncol(Fn)] #### If error is lower, reassign the values
+  Fn[N]        <- SA
+  Fn           <- cbind(Fn, chlv)
+  colnames(Fn) <- colnames(FALSE)
+  
+  return(NNLS_MF(Fn, S, cm))
 }
 
 
--- R/Random_neighbour2.R
+++ R/Random_neighbour2.R
@@ -17,41 +17,42 @@
 #' @examples
 #' @importFrom stats runif
 Random_neighbour2 <- function(Fn, Temp, chlv, s_c, place, S, cm, min.val, max.val){
-  s_c <- vectorise(s_c[,1:ncol(s_c)-1])
-  SE <-   Wrangling(Fn, min.val, max.val)[[3]] 
-  minF <- Wrangling(Fn, min.val, max.val)[[1]]
-  maxF <- Wrangling(Fn, min.val, max.val)[[2]]
-  ki <- maxF-minF
-  rand <- round(runif(n = length(s_c), -1, 1),4)
-  SA <- (SE + (Temp) *ki*rand)
-  SA <- as.vector(unlist(SA))
-  d <- which(SA < minF | SA > maxF)
-  length(d)
+  
+  s_c  <- vectorise(s_c[, -ncol(s_c)])
+  
+  wrangled <- Wrangling(Fn, min.val, max.val)
+  minF     <- wrangled[[1]]
+  maxF     <- wrangled[[2]]
+  SE       <- wrangled[[3]]
+  
+  rand <- round(runif(n = length(s_c), -1, 1), 4)
+  SA   <- SE + Temp * (maxF - minF) * rand
+
+  # F matrix indexes that are outside the range of min and max
+  d    <- which(SA < minF | SA > maxF) 
+  maxd <- maxF
+  mind <- minF
+  
   loop <- 1
-  while (length(d) >0){
-    loop = loop +1
-    nr <- round(runif(length(d),-1,1),4)
-    minr <- as.vector(unlist(minF))
-    maxr <- as.vector(unlist(maxF))
-    mind <- minr[d]
-    maxd <- maxr[d]
-    kir <- maxd-mind
-    SA2 <- (SE[d] +(Temp)*kir*nr)
-    SA[d] <- SA2 
-    d <- which(SA < minF | SA > maxF)
-    #print(loop)
-    if (loop > 50){
-      nn <- (minF[d]+maxF[d])/2
-      f <- round(runif(n=length(d),(minF[d]*1.20),(maxF[d]*0.80)),4)
-      SA[d] <- f
-      d <- which(SA < minF | SA > maxF)
+  while (length(d) > 0) {
+    loop  <- loop + 1
+    nr    <- round(runif(length(d), -1, 1), 4)
+    SA2   <- SE[d] + Temp * (maxd[d] - mind[d]) * nr
+    SA[d] <- SA2
+    d     <- which(SA < minF | SA > maxF)
+    
+    if (loop > 50) {
+      SA[d] <- round(runif(n = length(d), (minF[d] * 1.20), (maxF[d] * 0.80)), 4)
+      d     <- which(SA < minF | SA > maxF)
     }
   }
-  Fn <- Fn[,1:ncol(Fn)-1] #### If error is lower, reassign the values
-  Fn[Fn >0] <- SA
-  Fn <- cbind(Fn,chlv)
-  colnames(Fn) <- colnames(F)
-  F.n <- NNLS_MF(Fn, S, cm)
-  return(F.n)
+  
+  Fn           <- Fn[, -ncol(Fn)] #### If error is lower, reassign the values
+  Fn[Fn > 0]   <- SA
+  Fn           <- cbind(Fn, chlv)
+  colnames(Fn) <- colnames(FALSE)
+  
+  return(NNLS_MF(Fn, S, cm))
+
 }
 
