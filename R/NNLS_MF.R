a2022cb67ce5d511bcf2a6db64ee8c47821ab52a
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-07-07 16:06:05 -0400

    rm commented code

--- R/NNLS_MF.R
+++ R/NNLS_MF.R
@@ -41,19 +41,9 @@ NNLS_MF <- function(Fn, S, S_weights = NULL) {
     )
   C_new2 <- t(C_new2)
   Cn2    <- Normalise_S(C_new2) # Row sums to one
-  
-  # b <- crossprod(t(Weight_error(Fn, S_weights)), t(Weight_error(S, S_weights)))
-  # C_new2 <- t(RcppML::nnls(crossprod(t(Weight_error(Fn, S_weights))),
-  #   b,
-  #   cd_maxit = 1000, cd_tol = 1e-8
-  # ))
-  # C_new2 <- as.matrix(C_new2)
-  # Cn.s2  <- rowSums(C_new2)
-  # Cn2    <- C_new2 / Cn.s2 # Row sums to one
-  # Cn2    <- as.matrix(Cn2)
   colnames(Cn2) <- rownames(Fn)
   
-  # error <- Metrics::rmse(S,(C_new2%*%Fn))
+  # ---- calculate error term ---- #
   error <- sqrt(mean((S - C_new2 %*% Fn)^2)) # RMSE
   
   return(list("F matrix " = Fn, "RMSE" = error, "C matrix" = Cn2))
