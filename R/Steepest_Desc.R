3242c131450f961645661138ba3c6c84c24edbdd
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-07-09 13:01:38 -0400

    run `Steepest_Descent` instead of copying, to create a wrapper for algorithm

--- R/Steepest_Desc.R
+++ R/Steepest_Desc.R
@@ -22,37 +22,39 @@
 #' SDRes <- Steepest_Desc(Fnew,Snew, num.loops = 20)
 #' 
 Steepest_Desc <- function(Fmat, S, num.loops) {
-  S_Chl <- S[, ncol(S)]
-  cm    <- Bounded_weights(S, 30)
-  place <- which(Fmat[, -ncol(Fmat)] > 0)
+  S_Chl     <- S[, ncol(S)]
+  S_weights <- Bounded_weights(S, 30)
+  place     <- which(Fmat[, -ncol(Fmat)] > 0)
 
-  # loop      <- 1
-  F.new     <- NNLS_MF(Fmat, S, cm)
-  F.initial <- F.new
+  F_new <- Steepest_Descent(Fmat, place, S, S_weights, num.loops)
   
-  for (i in 1:num.loops) {
-    F.new  <- Minimise_elements_comb(F.initial[[1]], place, S, cm, c1_num = 3)
-    # loop   <- loop + 1
-    loop <- 1
-    while (F.new[[2]] > F.initial[[2]]) {
-      
-      if (loop <= 5) {
-        F.new <- Minimise_elements_comb(F.initial[[1]], place, S, cm, c1_num = 3)
-      } else if (loop < 10) {
-        F.new <- Minimise_elements_comb(F.initial[[1]], place, S, cm, c1_num = 1)
-      } else if (loop <= 100) {
-        # If it doesn't work the first time, it randomises at a lower rate
-        F.new <- Minimise_elements_comb(F.initial[[1]], place, S, cm, c1_num = 2)
-      } else if (loop > 100) {
-        # it will continue for 100 iterations, and then stop
-        break
-      }
-      loop <- loop + 1
-    }
-    F.initial <- F.new
-  }
-  F.new <- NNLS_MF_Final(as.matrix(F.new[[1]]), S, S_Chl, cm)
-  return(F.new)
+  
+  # F_new     <- NNLS_MF(Fmat, S, S_weights)
+  # F_initial <- F_new
+  # 
+  # for (i in 1:num.loops) {
+  #   F_new  <- Minimise_elements_comb(F_initial[[1]], place, S, S_weights, c1_num = 3)
+  #   loop <- 1
+  #   while (F_new[[2]] > F_initial[[2]]) {
+  # 
+  #     if (loop <= 5) {
+  #       F_new <- Minimise_elements_comb(F_initial[[1]], place, S, S_weights, c1_num = 3)
+  #     } else if (loop < 10) {
+  #       F_new <- Minimise_elements_comb(F_initial[[1]], place, S, S_weights, c1_num = 1)
+  #     } else if (loop <= 100) {
+  #       # If it doesn't work the first time, it randomises at a lower rate
+  #       F_new <- Minimise_elements_comb(F_initial[[1]], place, S, S_weights, c1_num = 2)
+  #     } else if (loop > 100) {
+  #       # it will continue for 100 iterations, and then stop
+  #       break
+  #     }
+  #     loop <- loop + 1
+  #   }
+  #   F_initial <- F_new
+  # }
+  
+  F_new <- NNLS_MF_Final(as.matrix(F_new[[1]]), S, S_Chl, S_weights)
+  return(F_new)
 }
 
 
