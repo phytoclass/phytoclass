3b5b3d7653d7434e5338ec1b1a3f430e8c021ac3
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-07-09 13:02:15 -0400

    rm commented code

--- R/Steepest_Desc.R
+++ R/Steepest_Desc.R
@@ -27,33 +27,8 @@ Steepest_Desc <- function(Fmat, S, num.loops) {
   place     <- which(Fmat[, -ncol(Fmat)] > 0)
 
   F_new <- Steepest_Descent(Fmat, place, S, S_weights, num.loops)
-  
-  
-  # F_new     <- NNLS_MF(Fmat, S, S_weights)
-  # F_initial <- F_new
-  # 
-  # for (i in 1:num.loops) {
-  #   F_new  <- Minimise_elements_comb(F_initial[[1]], place, S, S_weights, c1_num = 3)
-  #   loop <- 1
-  #   while (F_new[[2]] > F_initial[[2]]) {
-  # 
-  #     if (loop <= 5) {
-  #       F_new <- Minimise_elements_comb(F_initial[[1]], place, S, S_weights, c1_num = 3)
-  #     } else if (loop < 10) {
-  #       F_new <- Minimise_elements_comb(F_initial[[1]], place, S, S_weights, c1_num = 1)
-  #     } else if (loop <= 100) {
-  #       # If it doesn't work the first time, it randomises at a lower rate
-  #       F_new <- Minimise_elements_comb(F_initial[[1]], place, S, S_weights, c1_num = 2)
-  #     } else if (loop > 100) {
-  #       # it will continue for 100 iterations, and then stop
-  #       break
-  #     }
-  #     loop <- loop + 1
-  #   }
-  #   F_initial <- F_new
-  # }
-  
   F_new <- NNLS_MF_Final(as.matrix(F_new[[1]]), S, S_Chl, S_weights)
+  
   return(F_new)
 }
 
