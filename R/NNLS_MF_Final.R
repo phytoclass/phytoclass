844860e04b6a5aa3c968ed79f0ec05b51f3d7007
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-07-17 11:59:44 -0400

    attempt to fix error with R-CMD-check

--- R/NNLS_MF_Final.R
+++ R/NNLS_MF_Final.R
@@ -60,13 +60,14 @@ NNLS_MF_Final <- function(Fn, S, S_Chl, S_weights){
   
   # NULL assignment to stop NOTE during the package "Check"
   #  -  no visible binding for global variable
+  vals    <- NULL
+  row_num <- NULL
   
   # ---- plot final results ---- #
   colorBlindGrey8   <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
                          "#F0E442", "#0072B2", "#D55E00", "#CC79A7","#009E73",
                          "#001E73", "#013E73")
-  
-  vals <- NULL
+
   PLE  <- tidyr::pivot_longer(
     data      = cbind(Cn2, "row_num" = seq(nrow(Cn2))), 
     cols      = -row_num, 
@@ -75,7 +76,7 @@ NNLS_MF_Final <- function(Fn, S, S_Chl, S_weights){
     )
   
   plt <- 
-    ggplot2::ggplot(PLE, ggplot2::aes(x = row_num, y = vals, fill = names)) +
+    ggplot2::ggplot(PLE, ggplot2::aes_string(x = row_num, y = vals, fill = names)) +
     ggplot2::geom_area() +
     ggplot2::scale_color_manual(values = colorBlindGrey8) +
     ggplot2::scale_fill_manual(values = colorBlindGrey8) +
