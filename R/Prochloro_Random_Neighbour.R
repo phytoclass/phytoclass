1b0b9db7a6d7a97a4651317675cb831ec3ef0dbf
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-07-26 04:03:42 -0400

    rm commented code

--- R/Prochloro_Random_Neighbour.R
+++ R/Prochloro_Random_Neighbour.R
@@ -16,55 +16,6 @@
 #' @return
 #'
 #' @examples
-Prochloro_Random_Neighbour <- function(Fn, Temp, chlv, s_c, N, place, S, cm, min.val, max.val)
-{
-  k <- match(N, place)
-  s_c <- s_c[N]
-  SE <- Prochloro_Wrangling(Fn, min.val, max.val)[[3]]
-  SE <- SE[k]
-  minF <- Prochloro_Wrangling(Fn, min.val, max.val)[[1]]
-  minF <- minF[k]
-  maxF <- Prochloro_Wrangling(Fn, min.val, max.val)[[2]]
-  maxF <- maxF[k]
-  ki <- maxF - minF
-  rand <- round(runif(n = length(s_c), -1, 1), 4)
-  SA <- (SE + (Temp) * ki * rand)
-  SA <- as.vector(unlist(SA))
-  h <- which(maxF == 1)
-  if (length(h) > 0) {
-    SA[h] <- 1
-  }
-  d <- which(SA < minF | SA > maxF)
-  loop <- 1
-  max_loops <- 100
-  while (length(d) > 0) {
-    loop <- loop + 1
-    nr <- round(runif(length(d), -1, 1), 4)
-    minr <- as.vector(unlist(minF))
-    maxr <- as.vector(unlist(maxF))
-    mind <- minr[d]
-    maxd <- maxr[d]
-    kir <- maxd - mind
-    SA2 <- (SE[d] + (Temp) * kir * nr)
-    SA[d] <- SA2
-    d <- which(SA < minF | SA > maxF)
-    if (loop > max_loops) {
-      nn <- (minF[d] + maxF[d])/2
-      f <- round(runif(n = length(d), (minF[d] * 1.2),
-                       (maxF[d] * 0.8)), 4)
-      SA[d] <- f
-      d <- which(SA < minF | SA > maxF)
-    }
-  }
-  Fn <- Fn[, 1:ncol(Fn) - 1]
-  Fn[N] <- SA
-  Fn <- cbind(Fn, chlv)
-  colnames(Fn) <- colnames(S)
-  F.n <- NNLS_MF(Fn, S, cm)
-  return(F.n)
-}
-
-
 Prochloro_Random_Neighbour <- function(
   Fn, Temp, chlv, s_c, N, place, S, cm, min.val, max.val
   ) {
@@ -102,7 +53,6 @@ Prochloro_Random_Neighbour <- function(
       sort_min_max <- cbind(minF[oob] * 1.2, maxF[oob] * 0.80)
       sort_min_max <- t(apply(sort_min_max, 1, sort))
       p_new[oob]   <- round(runif(length(oob), sort_min_max[, 1], sort_min_max[, 2]), 4)
-      # p_new[oob]   <- round(runif(length(oob), minF[oob] * 1.2, maxF[oob] * 0.80), 4)
       oob          <- which(p_new < minF | p_new > maxF)
     }
   }
--- R/Prochloro_Random_Neighbour2.R
+++ R/Prochloro_Random_Neighbour2.R
@@ -18,59 +18,9 @@
 #' 
 #' @examples
 #' @importFrom stats runif
-# Prochloro_Random_Neighbour_2 <- function(Fn, Temp, chlv, s_c, place, S, cm, min.val, max.val,chlvp) 
-#   {
-#   s_c <- vectorise(s_c[, 1:ncol(s_c) - 1])
-#   s_c <- s_c[1:length(s_c)-1]
-#   SE <- Prochloro_Wrangling(Fn, min.val, max.val)[[3]]
-#   SE <- SE[1:length(SE)-1]
-#   minF <- Prochloro_Wrangling(Fn, min.val, max.val)[[1]]
-#   minF <- minF[1:length(minF)-1]
-#   maxF <- Prochloro_Wrangling(Fn, min.val, max.val)[[2]]
-#   maxF <- maxF[1:length(maxF)-1]
-#   ki <- maxF - minF
-#   rand <- round(runif(n = length(s_c), -1, 1), 4)
-#   SA <- (SE + (Temp) * ki * rand)
-#   SA <- as.vector(unlist(SA))
-#   
-#   d <- which(SA < minF | SA > maxF)
-#   loop <- 1
-#   max_loops <- 100 # Adjust this if needed
-#   
-#   while (length(d) > 0) {
-#     loop <- loop + 1
-#     nr <- round(runif(length(d), -1, 1), 4)
-#     minr <- as.vector(unlist(minF))
-#     maxr <- as.vector(unlist(maxF))
-#     mind <- minr[d]
-#     maxd <- maxr[d]
-#     kir <- maxd - mind
-#     SA2 <- (SE[d] + (Temp) * kir * nr)
-#     SA[d] <- SA2
-#     d <- which(SA < minF | SA > maxF)
-#     
-#     # Exit condition after max loops with a safe fallback
-#     if (loop > max_loops) {
-#       nn <- (minF[d]+maxF[d])/2
-#       f <- round(runif(n=length(d),(minF[d]*1.20),(maxF[d]*0.80)),4)
-#       SA[d] <- f
-#       d <- which(SA < minF | SA > maxF)
-#     }
-#   }
-#   
-#   
-#   Fn <- Fn[, 1:ncol(Fn) - 1]
-#   Fn <- Fn[, 1:ncol(Fn) - 1]
-#   Fn[Fn > 0] <- SA
-#   Fn <- cbind(Fn, chlvp)
-#   Fn <- cbind(Fn, chlv)
-#   colnames(Fn) <- colnames(S)
-#   F.n <- NNLS_MF(Fn, S, cm)
-#   return(F.n)
-# }
-
-
-Prochloro_Random_Neighbour_2 <- function(Fn, Temp, chlv, s_c,    place, S, cm, min.val, max.val, chlvp) {
+Prochloro_Random_Neighbour_2 <- function(
+  Fn, Temp, chlv, s_c, place, S, cm, min.val, max.val, chlvp
+  ) {
 
   s_c       <- vectorise(s_c[, -ncol(s_c)])
   s_c       <- s_c[-length(s_c)]
