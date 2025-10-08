dac7bccb1e20bd03676518ce79f3b70aa74f7dc7
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-10-08 00:58:27 -0400

    making more comparable to non-prochloro counterpart

--- R/Prochloro_Random_Neighbour.R
+++ R/Prochloro_Random_Neighbour.R
@@ -9,7 +9,7 @@
 #' @param s_c 
 #' @param k_idx 
 #' @param S 
-#' @param cm 
+#' @param S_weights 
 #' @param minF_vec 
 #' @param maxF_vec 
 #'
@@ -17,39 +17,54 @@
 #'
 #' @examples
 Prochloro_Random_Neighbour <- function(
-    Fn, Temp, chlv, chlvp, s_c, k_idx, S, cm,
+    Fn, Temp, chlv, chlvp, s_c, k_idx, S, S_weights,
     minF_vec, maxF_vec
-) {
+  ) {
+  
   core <- Fn[, 1:(ncol(Fn) - 2)]
   core_vec <- as.vector(core)
   
-  SE   <- core_vec[k_idx]
+  p_chg   <- core_vec[k_idx]
   minF <- minF_vec[k_idx]
   maxF <- maxF_vec[k_idx]
-  ki   <- maxF - minF
+  # ki   <- (maxF - minF)
   
-  rand <- round(runif(n = length(SE), -1, 1), 4)
-  SA   <- SE + Temp * ki * rand
+  # randomize pigment ratios
+  rand <- round(runif(n = length(p_chg), -1, 1), 4)
+  p_new   <- p_chg + Temp * (maxF - minF) * rand # new values for ratios
+  oob <- which(p_new < minF | p_new > maxF)      # out of bounds ratios
+  # d <- which(SA < minF | SA > maxF)
   
-  d <- which(SA < minF | SA > maxF)
-  loop <- 1; max_loops <- 100
-  while (length(d) > 0) {
+  loop <- 0
+  max_loops <- 100
+  while (length(oob) > 0) {
     loop <- loop + 1
-    nr <- round(runif(length(d), -1, 1), 4)
-    SA[d] <- SE[d] + Temp * (maxF[d] - minF[d]) * nr
-    d <- which(SA < minF | SA > maxF)
-    if (loop > max_loops) {
-      SA[d] <- (minF[d] + maxF[d]) / 2
-      d <- which(SA < minF | SA > maxF)
+    nr <- round(runif(length(oob), -1, 1), 4)
+    p_new[oob] <- p_chg[oob] + Temp * (maxF[oob] - minF[oob]) * nr
+    oob <- which(p_new < minF | p_new > maxF)
+    
+    # if (loop > max_loops) {
+    #   p_new[oob] <- (minF[oob] + maxF[oob]) / 2
+    #   oob <- which(p_new < minF | p_new > maxF)
+    # }
+    
+    if (loop > max_loops & length(oob) > 0) {
+      # sort bound limits for when bounds are small enough to overlap 
+      sort_min_max <- cbind(minF[oob] * 1.2, maxF[oob] * 0.80) 
+      sort_min_max <- t(apply(sort_min_max, 1, sort))
+      p_new[oob]   <- round(runif(length(oob), sort_min_max[, 1], sort_min_max[, 2]), 4)
+      oob          <- which(p_new < minF | p_new > maxF)
     }
+    
   }
   
-  v <- core_vec
-  v[k_idx] <- SA
-  core[] <- v
+  v        <- core_vec
+  v[k_idx] <- p_new
+  core[]   <- v
   
-  Fn <- cbind(core, chlvp, chlv)
-  colnames(Fn) <- colnames(S)
+  f_new           <- cbind(core, chlvp, chlv)
+  colnames(f_new) <- colnames(S)
   
-  NNLS_MF(Fn, S, cm)
+  return(NNLS_MF(f_new, S, S_weights))
+  
 }
