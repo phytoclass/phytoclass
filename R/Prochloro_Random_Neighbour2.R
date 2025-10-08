bac7ade477ecd867965df4f72caff2147e04d8d1
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-10-08 01:21:31 -0400

    update to match more closely non-prochloro

--- R/Prochloro_Random_Neighbour2.R
+++ R/Prochloro_Random_Neighbour2.R
@@ -8,7 +8,7 @@
 #' @param s_c xx
 #' @param place_full  xx
 #' @param S  xx
-#' @param cm  xx
+#' @param S_weights  xx
 #' @param minF_vec  xx
 #' @param maxF_vec  xx
 #' @param chlvp  xx
@@ -20,44 +20,55 @@
 #' @examples
 #' @importFrom stats runif
 Prochloro_Random_Neighbour_2 <- function(
-    Fn, Temp, chlv, s_c, place_full, S, cm,
+    Fn, Temp, chlv, s_c, place_full, S, S_weights,
     minF_vec, maxF_vec, chlvp, Fi_mask
-) {
+  ) {
   core <- Fn[, 1:(ncol(Fn) - 2)]
   mask_core <- Fi_mask[, 1:(ncol(Fi_mask) - 2)]
   
   core_vec <- as.vector(core)
   mask_vec <- as.vector(mask_core)
-  idx <- which(mask_vec > 0)
+  idx      <- which(mask_vec > 0)
   
-  SE   <- core_vec[idx]
-  minF <- minF_vec[idx]
-  maxF <- maxF_vec[idx]
-  ki   <- maxF - minF
+  p_chg <- core_vec[idx]
+  minF  <- minF_vec[idx]
+  maxF  <- maxF_vec[idx]
   
-  rand <- round(runif(n = length(SE), -1, 1), 4)
-  SA   <- SE + Temp * ki * rand
+  # randomize pigment ratios
+  rand  <- round(runif(n = length(p_chg), -1, 1), 4)
+  p_new <- p_chg + Temp * (maxF - minF) * rand # new values for ratios
+  oob   <- which(p_new < minF | p_new > maxF)  # out of bounds ratios
   
-  d <- which(SA < minF | SA > maxF)
-  loop <- 1; max_loops <- 100
-  while (length(d) > 0) {
-    loop <- loop + 1
-    nr <- round(runif(length(d), -1, 1), 4)
-    SA[d] <- SE[d] + Temp * (maxF[d] - minF[d]) * nr
-    d <- which(SA < minF | SA > maxF)
-    if (loop > max_loops) {
-      SA[d] <- (minF[d] + maxF[d]) / 2  # midpoint fallback
-      d <- which(SA < minF | SA > maxF)
+  loop      <- 0
+  max_loops <- 100
+  while (length(oob) > 0) {
+    loop       <- loop + 1
+    nr         <- round(runif(length(oob), -1, 1), 4)
+    p_new[oob] <- p_chg[oob] + Temp * (maxF[oob] - minF[oob]) * nr
+    oob        <- which(p_new < minF | p_new > maxF)
+    
+    # if (loop > max_loops) {
+    #   p_new[oob] <- (minF[oob] + maxF[oob]) / 2  # midpoint fallback
+    #   oob <- which(p_new < minF | p_new > maxF)
+    # }
+    
+    if (loop > max_loops & length(oob) > 0) {
+      # sort bound limits for when bounds are small enough to overlap 
+      sort_min_max <- cbind(minF[oob] * 1.2, maxF[oob] * 0.80) 
+      sort_min_max <- t(apply(sort_min_max, 1, sort))
+      p_new[oob]   <- round(runif(length(oob), sort_min_max[, 1], sort_min_max[, 2]), 4)
+      oob          <- which(p_new < minF | p_new > maxF)
     }
+    
   }
   
   # Write back ONLY to allowed cells
-  v <- core_vec
-  v[idx] <- SA
+  v      <- core_vec
+  v[idx] <- p_new
   core[] <- v
   
-  Fn <- cbind(core, chlvp, chlv)
-  colnames(Fn) <- colnames(S)
+  F_new <- cbind(core, chlvp, chlv)
+  colnames(F_new) <- colnames(S)
   
-  NNLS_MF(Fn, S, cm)
+  return(NNLS_MF(F_new, S, S_weights))
 }
