ca41469c126b326dd167460720c2391385988556
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-10-08 03:01:25 -0400

    error occurred in actions, changed name back but, should consider deleting this function all together

--- R/Prochloro_Sim_Anneal.R
+++ R/Prochloro_Sim_Anneal.R
@@ -24,9 +24,9 @@
 #' @examples
 #' # Using the built-in matrices Sp and Fp
 #' set.seed(5326)
-#' sa.example <- simulated_annealing_Prochloro1(Sp, Fp, niter = 5)
+#' sa.example <- simulated_annealing_Prochloro(Sp, Fp, niter = 5)
 #' sa.example$Figure
-simulated_annealing_Prochloro1 <- function (S, 
+simulated_annealing_Prochloro <- function(S, 
                                            Fmat = NULL,
                                            user_defined_min_max = NULL,
                                            do_matrix_checks = TRUE, 
