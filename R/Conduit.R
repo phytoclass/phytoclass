974e62e85bc432a2d8f61888058ce805ab3c83fa
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-10-14 12:24:03 -0400

    move old version of conduit to main conduit file and delete old function files

--- R/Conduit.R
+++ R/Conduit.R
@@ -23,3 +23,71 @@ Conduit <- function(Fmat, place, S, cm, c_num = c(1, 2, 3)) {
   res <- list(F.news[[1]], F.news[[2]], F.old)
   return(res)
 }
+
+
+#' #' Conduit between minimise_elements function and Fac_F_R 
+#' #' of steepest descent algorithm.
+#' #' 
+#' #' @keywords internal
+#' #'
+#' #' @param Fmat  xx   
+#' #' @param place  xx
+#' #' @param S  xx
+#' #' @param cm  xx
+#' #' @return
+#' #'
+#' #' @examples
+#' Conduit_1 <- function(Fmat, place, S, cm){
+#'   F.locs <- vector()
+#'   F.old <- NNLS_MF(Fmat, S, cm)
+#'   F.news <- Fac_F_RR1(F.old, place, S, cm)
+#'   F.new <- F.news[[1]]
+#'   n <- F.news[[2]]
+#'   res <- list(F.new,n, F.old)
+#'   return(res)
+#' }
+#' 
+#' #' Conduit between minimise_elements function and Fac_F_R 
+#' #' of steepest descent algorithm.
+#' #' 
+#' #' @keywords internal
+#' #' 
+#' #' @param Fmat    xx 
+#' #' @param place   xx
+#' #' @param S   xx
+#' #' @param cm   xx
+#' #'
+#' #' @return
+#' #'
+#' #' @examples
+#' Conduit_2 <- function(Fmat, place, S, cm){
+#'   F.locs <- vector()
+#'   F.old <- NNLS_MF(Fmat, S, cm)
+#'   F.news <- Fac_F_RR2(F.old, vary = place, place, S, cm)
+#'   F.new <- F.news[[1]]
+#'   n <- F.news[[2]]
+#'   res <- list(F.new, n, F.old)
+#'   return(res)
+#' }
+#' 
+#' #' Conduit between minimise_elements function and Fac_F_R 
+#' #' of steepest descent algorithm.
+#' #' 
+#' #' @keywords internal
+#' #' 
+#' #' @param Fmat   xx
+#' #' @param place xx
+#' #' @param S  xx
+#' #' @param cm xx
+#' #'
+#' #' @return
+#' #'
+#' #' @examples
+#' Conduit_3 <- function(Fmat, place, S, cm){
+#'   F.old <- NNLS_MF(Fmat, S, cm)
+#'   F.news <- Fac_F_RR3(F.old, vary = place, place, S, cm)
+#'   F.new <- F.news[[1]]
+#'   n <- F.news[[2]]
+#'   res <- list(F.new, n, F.old)
+#'   return(res)
+#' }
