24d2708134cd9517ee700f7b471442294e541f74
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2025-10-14 12:31:29 -0400

    move all old versions of Fac_F_RR to main version and delete old files

--- R/Fac_F_RR.R
+++ R/Fac_F_RR.R
@@ -96,3 +96,164 @@ Fac_F_RR <- function(Fmat, vary, S, cm, fac_rr = c(1, 2, 3), place = NULL) {
   res <- list(F.new, cont)
   return(res)
 }
+
+# ============================================================================ #
+# ---- old versions ---- #
+# ============================================================================ #
+
+#' #' Part of the steepest descent algorithm and work to reduce error given 
+#' #' the S and F matrices.
+#' #' 
+#' #' @keywords internal 
+#' #'
+#' #' @param Fmat XX
+#' #' @param vary XX
+#' #' @param S XX
+#' #' @param cm XX
+#' #'
+#' #' @return
+#' #'
+#' #' @examples
+#' # Inputs are F and which elements to vary, should be all elements
+#' Fac_F_RR1 <- function(Fmat, vary, S, cm){ 
+#'   F.locs <- vector() 
+#'   Fs <- Fmat[[1]]
+#'   F.new <- lapply(vary, function(i){ # randomises every element in 'vary' 
+#'     Replace_Rand(Fmat, i, S, cm, min.scaler = 0.99, max.scaler = 1.01)})
+#'   # Shows which elements reduce error  
+#'   cont <- lapply(1:length(F.new), function(i){ c <- which(length(F.new[[i]]) == 4)}) 
+#'   conts <- which(cont==1)
+#'   # Procedure for if no elements reduce error  
+#'   if(!is.null(length(conts))){ 
+#'     cont <- as.list(vary[conts])
+#'     conts <- as.list(conts)
+#'     Locs <-cont
+#'     F.news <- sapply(conts, function(i) {
+#'       sapply(cont, function(j){
+#'         F.locs[[length(F.locs)+1]] <- F.new[[i]][[1]][[j]]
+#'       })
+#'     })
+#'     if (length(F.news) > 0){
+#'       if (length(F.news) >1 ){F.news <- diag(F.news)}
+#'       else{F.news <- F.news[[1]]}      
+#'       cont <- unlist(cont)
+#'       F.new <- replace(Fmat[[1]],cont,F.news)
+#'       F.new <- NNLS_MF(F.new, S, cm)
+#'     }
+#'     else{
+#'       F.new <- NNLS_MF(Fmat[[1]], S, cm)
+#'       cont <- vary
+#'     }
+#'   }
+#'   else{
+#'     F.new <- NNLS_MF(Fmat[[1]], S, cm)
+#'     cont <- vary
+#'   }
+#'   res <- list(F.new,cont)
+#'   return(res)
+#'   
+#' }
+#' 
+#' #' Part of the steepest descent algorithm and work to reduce error given 
+#' #' the S and F matrices
+#' #' 
+#' #' @keywords internal
+#' #'
+#' #' @param Fmat   xx
+#' #' @param vary    xx
+#' #' @param place   xx
+#' #' @param S   xx
+#' #' @param cm   xx
+#' #'
+#' #' @return
+#' #'
+#' #' @examples
+#' Fac_F_RR2 <- function(Fmat, vary, place, S, cm){
+#'   F.locs <- vector()
+#'   F.new <- lapply(vary, function(i){ 
+#'     Replace_Rand(Fmat,i, S, cm, min.scaler = 0.98, max.scaler = 1.02)})
+#'   cont <- lapply(1:length(F.new), function(i){ c <- which(length(F.new[[i]]) == 4)})
+#'   conts <- which(cont==1)
+#'   if(!is.null(length(conts))){
+#'     cont <- as.list(vary[conts])
+#'     conts <- as.list(conts)
+#'     Locs <-cont
+#'     F.news <- sapply(conts, function(i) {
+#'       sapply(cont, function(j){
+#'         F.locs[[length(F.locs)+1]] <- F.new[[i]][[1]][[j]]
+#'       })
+#'     })
+#'     if (length(F.news) > 0){
+#'       if (length(F.news) >1 ){F.news <- diag(F.news)}
+#'       else{F.news <- F.news[[1]]} 
+#'       cont <- unlist(cont)
+#'       F.new <- replace(Fmat[[1]],cont,F.news)
+#'       F.new <- NNLS_MF(F.new,S,cm)
+#'     }
+#'     else{
+#'       C <- Fac_F_RR1(Fmat, place, S, cm)
+#'       F.new <- C[[1]] 
+#'       cont <- C[[2]]
+#'     }
+#'   }
+#'   else{
+#'     C <- Fac_F_RR1(Fmat, place, S, cm)
+#'     F.new <- C[[1]] 
+#'     cont <- C[[2]]
+#'   }
+#'   res <- list(F.new,cont)
+#'   return(res)
+#'   
+#' }
+#' 
+#' #' Part of the steepest descent algorithm and work to reduce error given 
+#' #' the S and F matrices
+#' #' 
+#' #' @keywords internal
+#' #'
+#' #' @param Fmat  xx
+#' #' @param vary xx
+#' #' @param place   xx   
+#' #' @param S  xx
+#' #' @param cm xx
+#' #'
+#' #' @return
+#' #'
+#' #' @examples
+#' Fac_F_RR3 <- function(Fmat, vary, place, S, cm){
+#'   F.locs <- vector()
+#'   F.new <- lapply(vary, function(i) {Replace_Rand(Fmat, i, S, cm, min.scaler = 0.97, max.scaler = 1.03)})
+#'   cont <- lapply(1:length(F.new), function(i){ c <- which(length(F.new[[i]]) == 4)})
+#'   conts <- which(cont==1)
+#'   if(!is.null(length(conts))){
+#'     cont <- as.list(vary[conts])
+#'     conts <- as.list(conts)
+#'     Locs <-cont
+#'     F.news <- sapply(conts, function(i) {
+#'       sapply(cont, function(j){
+#'         F.locs[[length(F.locs)+1]] <- F.new[[i]][[1]][[j]]
+#'       })
+#'     })
+#'     if (length(F.news) > 0){
+#'       if (length(F.news) >1 ){F.news <- diag(F.news)}
+#'       else{F.news <- F.news[[1]]}
+#'       cont <- unlist(cont)
+#'       F.new <- replace(Fmat[[1]],cont,F.news)
+#'       F.new <- NNLS_MF(F.new, S, cm)
+#'     }
+#'     else{
+#'       C <- Fac_F_RR2(Fmat, vary, place, S, cm)
+#'       F.new <- C[[1]] 
+#'       cont <- C[[2]]
+#'     }
+#'   }
+#'   else{
+#'     C <- Fac_F_RR1(Fmat, place)
+#'     F.new <- C[[1]] 
+#'     cont <- C[[2]]
+#'   }
+#'   res <- list(F.new,cont)
+#'   return(res)
+#'   
+#' }
+
