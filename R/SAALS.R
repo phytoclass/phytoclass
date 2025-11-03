1c139604acc9ef46ac5aa06a8cd25957abc6db6c
Tylar <murray.tylar@gmail.com>
2025-11-03 15:52:16 -0500

    update docstring

--- R/SAALS.R
+++ R/SAALS.R
@@ -1,11 +1,11 @@
 #' Apply the steepest descent algorithm to optimize pigment ratios
-#' in phytoplankton classification
+#' in phytoplankton classification. Loosely wraps Seepest_Descent function.
 #'  
 #' @keywords internal  
 #'
 #' @param Ft Initial F matrix containing pigment ratios
-#' @param min.value Minimum allowed values for pigment ratios
-#' @param max.value Maximum allowed values for pigment ratios
+#' @param min.value UNUSED?
+#' @param max.value UNUSED?
 #' @param place Vector of indices where F matrix has non-zero values
 #' @param S Matrix of sample measurements
 #' @param cm Vector of bounded weights for each pigment
@@ -13,20 +13,16 @@
 #'
 #' @return A list containing:
 #'   \item{[[1]]}{The optimized F matrix}
-#'   \item{[[2]]}{Vector of error values over iterations}
+#'   \item{[[2]]}{Final RMSE value}
 #'
 #' @examples
-#' # Create sample matrices
-#' Ft <- matrix(c(0.5, 0.3, 0.2,
-#'                0.4, 0.1, 0.5), nrow=2, byrow=TRUE)
-#' min.value <- matrix(0.1, nrow=2, ncol=3)
-#' max.value <- matrix(0.9, nrow=2, ncol=3)
-#' place <- c(1, 2, 3, 4, 5, 6)  # all elements
-#' S <- matrix(runif(12), nrow=4)  # 4 samples, 3 pigments
-#' cm <- c(1, 1, 1)  # equal weights
-#' 
-#' # Run optimization
-#' result <- SAALS(Ft, min.value, max.value, place, S, cm, num.loops=100)
+#'  Fmat <- as.matrix(phytoclass::Fm)
+#'  S <- as.matrix(phytoclass::Sm)
+#'  S_weights <- as.numeric(phytoclass:::Bounded_weights(S))
+#'  place <- which(Fmat[, seq(ncol(Fmat) - 2)] > 0)
+#'  num.loops <- 2
+#'  # Run SAALS
+#'  result <- phytoclass:::SAALS(Fmat, place, S, S_weights, num.loops)
 
 SAALS <- function(Ft, min.value, max.value, place, S, cm, num.loops){
   g <- Steepest_Descent(Ft, place, S, cm, num.loops)
